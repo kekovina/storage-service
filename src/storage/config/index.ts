f7efa91a02321d2c61f5ccc26e9bddf4e17ec024
Anton Azarov <a.azarov@3205.team>
2025-09-20 20:55:57 +0300

    fix: refactoring

--- src/storage/config/index.ts
+++ src/storage/config/index.ts
@@ -3,5 +3,3 @@ import path from 'node:path';
 export const ROOT_PATH = process.cwd();
 
 export const FOLDER_PATH = path.join(ROOT_PATH, 'uploads');
-
-export const MAX_FILE_SIZE = 5 * 1024 * 1024; // 5MB
